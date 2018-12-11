91454e1c7dbc717db5bdaad834e0d00416ab4a2a
Takuya OHASHI <tky.ohashi@gmail.com>
2018-12-11 14:38:08 +0900

    add sample

--- lang-tour/classes/extends.dart
+++ lang-tour/classes/extends.dart
@@ -10,9 +10,25 @@ class SmartTelevision extends Television {
     super.turnOn();
     print("smart television turn on");
   }
+
+  @override
+  void noSuchMethod(Invocation invocation) {
+    print('a ${invocation.memberName}');
+  }
+}
+
+/* noSuchMethod を使いたいが、思うように動かない */
+class CarTelevision  {
+  @override
+  void noSuchMethod(Invocation invocation) {
+    print('a ${invocation.memberName}');
+  }
 }
 
 void main() {
   var smartTV = SmartTelevision();
   smartTV.turnOn();
+  
+  var carTV = CarTelevision();
+  carTV.unko();
 }
